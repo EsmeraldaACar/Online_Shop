a728ad57b1136369ddfd3e95aedb925a851cc251
EsmeraldaACar <63943574+EsmeraldaACar@users.noreply.github.com>
2023-09-11 19:07:42 -0600

    freeze arreglo de productos y mejora en la exportacion de la constante

--- src/constants/products.js
+++ src/constants/products.js
@@ -1,7 +1,8 @@
-export const productos = [
+const PRODUCTS = Object.freeze([
   {
     SKU: 'SKU123',
     nombre: 'Producto 1',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 10,
     precio: 29.99,
     imagen: 'URL_IMAGEN_1'
@@ -9,6 +10,7 @@ export const productos = [
   {
     SKU: 'SKU456',
     nombre: 'Producto 2',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 5,
     precio: 49.99,
     imagen: 'URL_IMAGEN_2'
@@ -16,6 +18,7 @@ export const productos = [
   {
     SKU: 'SKU789',
     nombre: 'Producto 3',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 20,
     precio: 9.99,
     imagen: 'URL_IMAGEN_3'
@@ -23,6 +26,7 @@ export const productos = [
   {
     SKU: 'SKU1011',
     nombre: 'Producto 4',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 15,
     precio: 19.99,
     imagen: 'URL_IMAGEN_4'
@@ -30,6 +34,7 @@ export const productos = [
   {
     SKU: 'SKU1213',
     nombre: 'Producto 5',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 8,
     precio: 39.99,
     imagen: 'URL_IMAGEN_5'
@@ -37,6 +42,7 @@ export const productos = [
   {
     SKU: 'SKU1415',
     nombre: 'Producto 6',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 12,
     precio: 14.99,
     imagen: 'URL_IMAGEN_6'
@@ -44,6 +50,7 @@ export const productos = [
   {
     SKU: 'SKU1617',
     nombre: 'Producto 7',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 30,
     precio: 8.99,
     imagen: 'URL_IMAGEN_7'
@@ -51,6 +58,7 @@ export const productos = [
   {
     SKU: 'SKU1819',
     nombre: 'Producto 8',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 3,
     precio: 64.99,
     imagen: 'URL_IMAGEN_8'
@@ -58,6 +66,7 @@ export const productos = [
   {
     SKU: 'SKU2021',
     nombre: 'Producto 9',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 18,
     precio: 22.99,
     imagen: 'URL_IMAGEN_9'
@@ -65,9 +74,12 @@ export const productos = [
   {
     SKU: 'SKU2223',
     nombre: 'Producto 10',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
     cantidad: 25,
     precio: 17.99,
     imagen: 'URL_IMAGEN_10'
   }
-];
+]);
+
+export default PRODUCTS
 
